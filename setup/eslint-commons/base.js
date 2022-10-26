d41223659540a6f0c8d64b1384c4bf003e3d0fcc
José Andersson <jamfreire@gmail.com>
2022-10-26 16:14:35 -0300

    Setup test-commons

--- setup/eslint-commons/base.js
+++ setup/eslint-commons/base.js
@@ -1,4 +1,12 @@
 module.exports = {
-  extends: ["next", "eslint:recommended", "plugin:prettier/recommended"],
+  env: {
+    "jest/globals": true,
+  },
+  extends: [
+    "next",
+    "eslint:recommended",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
+  ],
   rules: { "no-console": "error", "@next/next/no-html-link-for-pages": "off" },
 };
