36b2e27b96e773316443475bbad7e6696e8353fb
José Andersson <jamfreire@gmail.com>
2022-10-26 11:15:29 -0300

    Setup tsconfig-commons e styled-components

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,7 +1,13 @@
+import { Text } from "@alura/design-system/components/Text";
+import { sum } from "@alura/utils/math/sum";
+
 export default function HomeScreen() {
   return (
     <main>
-      <h1>Home</h1>
+      <Text tag="h1">Home</Text>
+      <Text tag="p">
+        Importando módulo local: @alura/utils/math/sum sum(2,2): {sum(2, 2)}
+      </Text>
     </main>
   );
 }
